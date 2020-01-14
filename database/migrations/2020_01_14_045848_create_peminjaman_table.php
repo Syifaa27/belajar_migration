2ecfd8b10d63337946c42859c329a47591abc565
Syifaa27 <syifa_auliya_27rpl@smktelkom-mlg.sch.id>
2020-01-14 14:19:44 +0700

    belajar_migration

---
+++ database/migrations/2020_01_14_045848_create_peminjaman_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreatePeminjamanTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('peminjaman', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->date('tanggal_pinjam');
+            $table->integer('id_anggota');
+            $table->integer('id_petugas');
+            $table->date('tanggal_deadline');
+            $table->integer('denda');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('peminjaman');
+    }
+}
